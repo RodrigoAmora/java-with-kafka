384f01fedfc58773da4e34608f9a9086e3c8b0ec
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-07-09 00:54:04 -0300

    Commit do projeto

---
+++ shop-api-consumer/src/test/java/br/com/rodrigoamora/consumer/ShopApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
