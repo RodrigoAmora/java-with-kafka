46b36d78f8c3ffb3e89145876d3c802cce7dae40
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-01-22 01:36:55 -0300

    Interface ShopRepository

---
+++ shop-api-producer/src/main/java/br/com/rodrigoamora/producer/repository/ShopRepository.java
@@ -0,0 +1,11 @@
+package br.com.rodrigoamora.producer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.rodrigoamora.producer.entity.Shop;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+}
