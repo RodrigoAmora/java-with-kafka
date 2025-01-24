d066f56efc4663100d6b1252fa94a0e94ac84881
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-01-24 17:22:05 -0300

    Classe de modelo Product

---
+++ shop-api-consumer/src/main/java/br/com/rodrigoamora/consumer/entity/Product.java
@@ -0,0 +1,24 @@
+package br.com.rodrigoamora.consumer.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "product_identifier")
+	private String identifier;
+	
+	private Integer amount;
+}
