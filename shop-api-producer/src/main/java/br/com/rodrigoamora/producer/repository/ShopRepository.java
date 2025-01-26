58934fff5d599306dffa14cf35717f99a743be80
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-01-26 02:19:22 -0300

    Método findByIdentifier

--- shop-api-producer/src/main/java/br/com/rodrigoamora/producer/repository/ShopRepository.java
+++ shop-api-producer/src/main/java/br/com/rodrigoamora/producer/repository/ShopRepository.java
@@ -7,5 +7,5 @@ import br.com.rodrigoamora.producer.entity.Shop;
 
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Long> {
-
+	public Shop findByIdentifier(String identifier);
 }
