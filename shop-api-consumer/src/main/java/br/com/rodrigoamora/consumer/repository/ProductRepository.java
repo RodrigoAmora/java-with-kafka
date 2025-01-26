a50c79df82ae542a5e04643077e3e3c1f76ebe54
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-01-26 02:20:26 -0300

    Adicionando a anotação Repository

--- shop-api-consumer/src/main/java/br/com/rodrigoamora/consumer/repository/ProductRepository.java
+++ shop-api-consumer/src/main/java/br/com/rodrigoamora/consumer/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package br.com.rodrigoamora.consumer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.rodrigoamora.consumer.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Product findByIdentifier(String identifier);
